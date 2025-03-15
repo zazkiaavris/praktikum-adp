a0c0d71133c908cc5d678b87cbd61dba2af7b8c9
zazkiaavris <zazkiaavris@gmail.com>
2025-03-15 12:42:57 +0700

    Add files via upload

---
+++ tugas2zazkia.py
@@ -0,0 +1,29 @@
+angka_pertama=int(input("masukkan angka pertama :"))
+angka_kedua = int(input("masukkan angka kedua :"))
+print("pilihan operasi")
+print("1. penjumlahan")
+print("2. pengurangan")
+print("3. perkalian")
+print("4. pembagian")
+print("5. keluar ")
+nomor_pilihan= int(input("masukkan nomor pilihan operasi (1/2/3/4/5):"))
+perhitungan = True
+while perhitungan :
+    if nomor_pilihan == 1:
+        hasil= angka_pertama + angka_kedua
+        print(f"{hasil}")
+    elif nomor_pilihan == 2 :
+        hasil = angka_pertama - angka_kedua
+        print(f"{hasil}")
+    elif nomor_pilihan == 3:
+        hasil = angka_pertama*angka_kedua
+        print(f"{hasil}")
+    elif nomor_pilihan == 4:
+        if angka_kedua == 0 :
+            print("tidak valid")
+        else :
+            hasil = angka_pertama / angka_kedua 
+            print(f"{hasil}")
+    else :
+        print("pilihan operasi tidak valid = keluar")
+    perhitungan = False
